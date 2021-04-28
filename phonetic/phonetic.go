1358e8427343906ead06234c3ab8684c85d577d4
Matthew Connelly <matthew.b.connelly@gmail.com>
2021-04-28 01:52:47 -0400

    clarifying comments to phonetic functions, minor loop refactor

--- phonetic/phonetic.go
+++ phonetic/phonetic.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-// mapping from ints 0-9 to strings
+// Mapping from ints 0-9 to strings
 type PhoneticDigits [10]string
 
 var Digits PhoneticDigits = [10]string{
@@ -23,28 +23,32 @@ var Digits PhoneticDigits = [10]string{
 	"Nine",
 }
 
+// PhoneticizeInt will take an integer and return a string without spaces
+// representing the phonetic equivalent of that integer, as well as an error.
 func PhoneticizeInt(n int) (string, error) {
 	if n < 0 {
 		return "", errors.New(fmt.Sprintf("Cannot phoneticize %d. Must be positive.\n", n))
 	}
-	length := int(math.Log10(float64(n))) + 1
-	output := make([]string, length)
-	i := length - 1
 
-	for i >= 0 {
+	// Lenght of integer.
+	length := int(math.Log10(float64(n))) + 1
+
+	output := make([]string, length)
+
+	for i := length - 1; i >= 0; i-- {
+		// Get last/least significant digit.
 		d := n % 10
-		str, err := phoneticizeDigit(d)
-		if err != nil {
-			return "", errors.New(fmt.Sprintf("Cannot phoneticize integer %d. %s", n, err.Error()))
-		}
+
+		str, _ := phoneticizeDigit(d)
+
 		output[i] = str
 		n /= 10
-		i--
 	}
 
 	return strings.Join(output, ""), nil
 }
 
+// Look up digit in Digits array.
 func phoneticizeDigit(n int) (string, error) {
 	if n < 0 || n > 9 {
 		return "", errors.New(fmt.Sprintf("Cannot phoneticize digit %d, must be in range 0-9.\n", n))
